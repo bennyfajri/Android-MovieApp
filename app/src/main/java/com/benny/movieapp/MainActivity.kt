5c6efc566966efb354f605278a8d18a6160e8fa4
bennyfajri <m.b.fajrii@gmail.com>
2021-07-08 23:28:29 +0700

    add Layout and Model

--- app/src/main/java/com/benny/movieapp/MainActivity.kt
+++ app/src/main/java/com/benny/movieapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.benny.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
