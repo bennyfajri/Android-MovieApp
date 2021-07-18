91f7db4d944cd9e38d7a982ce33d89811761787a
bennyfajri <m.b.fajrii@gmail.com>
2021-07-18 22:09:40 +0700

    add load state adapter

--- app/src/main/java/com/benny/movieapp/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/benny/movieapp/ui/movie/MovieFragment.kt
@@ -5,9 +5,11 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.widget.SearchView
+import androidx.core.view.isVisible
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import com.benny.movieapp.R
 import com.benny.movieapp.databinding.FragmentMovieBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,12 +26,33 @@ class MovieFragment : Fragment(R.layout.fragment_movie) {
 
         _binding = FragmentMovieBinding.bind(view)
         val adapter = MovieAdapter()
+
         binding.apply {
             rvMovie.setHasFixedSize(true)
             rvMovie.adapter = adapter.withLoadStateHeaderAndFooter(
                 header = MovieLoadStateAdapter{adapter.retry()},
                 footer = MovieLoadStateAdapter {adapter.retry()}
             )
+            btnTryAgain.setOnClickListener {
+                adapter.retry()
+            }
+        }
+        
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                rvMovie.isVisible = loadState.source.refresh is LoadState.NotLoading
+                btnTryAgain.isVisible = loadState.source.refresh is LoadState.Error
+                tvFailed.isVisible = loadState.source.refresh is LoadState.Error
+
+                //not found
+                if(loadState.source.refresh is LoadState.NotLoading && loadState.append.endOfPaginationReached && adapter.itemCount < 1){
+                    rvMovie.isVisible = false
+                    tvNotFound.isVisible = true
+                }else{
+                    tvNotFound.isVisible = false
+                }
+            }
         }
 
         viewModel.movies.observe(viewLifecycleOwner){
